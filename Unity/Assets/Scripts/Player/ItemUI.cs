d32b3ec0d6e7e943d2d5ddff3a4261bd116d72a4
naoyuki0223 <naoyuki02232525@gmail.com>
2021-06-01 22:59:48 +0900

    a

---
+++ Unity/Assets/Scripts/Player/ItemUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemUI : MonoBehaviour
+{
+    [SerializeField] GameObject ItemPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ItemPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("i"))
+        {
+            ItemPanel.SetActive(!ItemPanel.activeSelf);
+        }
+    }
+}
